e82b126b34d881fd91d1a4fb0332ecaa411ee1ca
SAMMYBOOOOM <1647496367sam@gmail.com>
2025-02-09 17:04:05 +0800

    initial commit

---
+++ src/views/Element.vue
@@ -0,0 +1,210 @@
+<template>
+  <div>
+
+    <el-menu router style="border: none" :default-active="$route.path">
+      <el-menu-item index="/">
+        <template slot="title">
+          <i class="el-icon-house"></i>
+          <span>Home page</span>
+        </template>
+      </el-menu-item>
+      <el-menu-item index="/element">Element</el-menu-item>
+      <el-menu-item>Home</el-menu-item>
+      <el-menu-item>Home</el-menu-item>
+      <el-submenu>
+        <template slot="title">
+          <i class="el-icon-menu"></i>
+          <span>Management</span>
+        </template>
+        <el-menu-item>User information</el-menu-item>
+      </el-submenu>
+    </el-menu>
+    </el-aside>
+
+    <!--    <el-row gutter="10">-->
+    <!--      <el-col :span="6">-->
+    <!--        <div style="width: 100%; height: 300px; background-color: dodgerblue;"></div>-->
+    <!--      </el-col>-->
+    <!--      <el-col :span="6">-->
+    <!--        <div style="width: 100%; height: 300px; background-color: pink;"></div>-->
+    <!--      </el-col>-->
+    <!--    </el-row>-->
+    <el-row>
+      <el-col :span="6">
+        <div style="padding: 10px; border: 1px solid #ccc; text-align: center">
+          <img style="width: 80%" src="@/assets/logo.png" alt="">
+          <div style="text-align: center">This is something</div>
+          <div style="color: red">Price: 200</div>
+        </div>
+      </el-col>
+      <el-col :span="6">
+        <div style="padding: 10px; border: 1px solid #ccc; text-align: center">
+          <img style="width: 80%" src="@/assets/logo.png" alt="">
+          <div style="text-align: center">This is something</div>
+          <div style="color: red">Price: 200</div>
+        </div>
+      </el-col>
+      <el-col :span="6">
+        <div style="padding: 10px; border: 1px solid #ccc; text-align: center">
+          <img style="width: 80%" src="@/assets/logo.png" alt="">
+          <div style="text-align: center">This is something</div>
+          <div style="color: red">Price: 200</div>
+        </div>
+      </el-col>
+      <el-col :span="6">
+        <div style="padding: 10px; border: 1px solid #ccc; text-align: center">
+          <img style="width: 80%" src="@/assets/logo.png" alt="">
+          <div style="text-align: center">This is something</div>
+          <div style="color: red">Price: 200</div>
+        </div>
+      </el-col>
+    </el-row>
+
+    <el-row>
+      <el-col :span="24">
+        <button>Normal button</button>
+        <el-button>Element button</el-button>
+        <el-button type="primary">Primary</el-button>
+        <el-button type="danger" plain round>Danger</el-button>
+        <el-button type="warning" circle>Warning</el-button>
+        <el-button type="info">Info</el-button>
+        <el-button type="success">Success</el-button>
+        <el-button circle icon="el-icon-search"></el-button>
+        <el-button circle icon="el-icon-edit" type="primary"></el-button>
+      </el-col>
+    </el-row>
+
+    <el-row style="top: 20px">
+      <el-col>
+        <el-input style="width: 200px" v-model="value" placeholder="Please input"></el-input>
+        <el-input type="textarea" style="width: 200px" v-model="value" placeholder="Please input"></el-input>
+        <el-input show-password style="width: 200px" v-model="value" placeholder="Please input"></el-input>
+        <el-input clearable style="width: 200px" v-model="value" placeholder="Please input"
+                  suffix-icon="el-icon-search"></el-input>
+      </el-col>
+    </el-row>
+
+    <el-row style="margin: 20px 0">
+      <el-autocomplete style="width: 300px" placeholder="Search" :fetch-suggestions="querySearch"
+                       :trigger-on-focus="false" v-model="value1"></el-autocomplete>
+    </el-row>
+
+    <el-row style="top: 20px;">
+      <el-select v-model="select" @change="changeSelect" placeholder="Please select">
+        <el-option value="1"></el-option>
+        <el-option value="2"></el-option>
+        <el-option value="3"></el-option>
+
+      </el-select>
+      <el-select v-model="select" @change="changeSelect" placeholder="Please select">
+        <el-option v-for="item in fruits" :key="item.id" :label="item.name" :value="item.id"></el-option>
+      </el-select>
+
+      <el-select v-model="select1" @change="changeSelect1" placeholder="Please select" multiple filterable>
+        <el-option v-for="item in users" :key="item.card" :label="item.name" :value="item.card"></el-option>
+      </el-select>
+    </el-row>
+
+    <el-row style="margin: 20px 0">
+      <el-radio-group v-model="radio" @change="changeRadio">
+        <el-radio label="1">Radio 1</el-radio>
+        <el-radio label="2">Radio 2</el-radio>
+      </el-radio-group>
+
+      <el-checkbox-group v-model="checkList" @change="changeSelectRadio">
+        <el-checkbox label="multi 1"></el-checkbox>
+        <el-checkbox label="multi 2"></el-checkbox>
+      </el-checkbox-group>
+
+      <el-date-picker v-model="date" type="date" placeholder="Pick a date" value-format="MM-dd-yyyy"
+                      @change="changeDate" style="width: 300px"></el-date-picker>
+      <el-date-picker v-model="datetime" type="datetime" placeholder="Pick a date" value-format="MM-dd-yyyy HH:mm:ss"
+                      @change="changeDateTime" style="width: 300px"></el-date-picker>
+
+      <el-date-picker v-model="daterange" type="daterange" range-separator="To" start-placeholder="Start date"
+                      end-placeholder="End date" style="width: 300px"></el-date-picker>
+
+    </el-row>
+
+    <el-row style="top: 20px">
+      <el-table :data="tableData" border :header-cell-style="{background: 'aliceblue', fontSize: '20px'}">
+        <el-table-column label="identity" prop="id" align="center"></el-table-column>
+        <el-table-column label="name" prop="name" align="center"></el-table-column>
+        <el-table-column label="address" prop="address" align="center"></el-table-column>
+        <el-table-column label="age" prop="age" align="center"></el-table-column>
+        <el-table-column label="edit">
+          <template v-slot="scope">
+            <el-button type="primary" @click="edit(scope.row)">Edit</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-row>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Element',
+  data() {
+    return {
+      tableData: [{name: 'Sam', address: '123', id: 1, age: 20}, {
+        name: 'Tom',
+        address: '456',
+        id: 2,
+        age: 21
+      }, {name: 'Lily', address: '789', id: 3, age: 22}],
+      value: '',
+      value1: '',
+      coffees: [{value: '1black'}, {value: '1white'}, {value: '2latte'}, {value: '3cappuccino'}],
+      select: '',
+      select1: '',
+      fruits: [{name: 'apple', id: 1}, {name: 'banana', id: 2}, {name: 'orange', id: 3}],
+      users: [{name: 'Sam', card: '123'}, {name: 'Tom', card: '456'}, {name: 'Lily', card: '789'}],
+      radio: '',
+      checkList: [],
+      date: '',
+      datetime: '',
+      daterange: ''
+    }
+  },
+  methods: {
+    querySearch(query, cb) {  // callback
+      // console.log(query)
+      let result = query ? this.coffees.filter(v => v.value.includes(query)) : this.coffees
+      cb(result)
+    },
+    changeSelect() {
+      console.log(this.select)
+    },
+    changeSelect1() {
+      console.log(this.select1)
+    },
+    changeRadio() {
+      console.log(this.radio)
+    },
+    changeSelectRadio() {
+      console.log(this.checkList)
+    },
+    changeDate() {
+      console.log(this.date)
+    },
+    changeDateTime() {
+      console.log(this.datetime)
+    },
+    edit(row) {
+      // alert(row.name)
+      this.$message.success(row.name)
+      // this.$notify.warning(row.name)
+      this.$confirm("Are you sure to delete this item?", "Warning", {
+        type: 'warning'
+      }).then(res => {
+        this.$message.success("you click ok")
+      }).catch(() => {
+        this.$message.info("you click cancel")
+      })
+    }
+
+  }
+}
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,152 @@
+<template>
+  <div>
+    <el-container>
+      <!--      left manu-->
+      <el-aside :width="asideWidth" style="min-height: 100vh; background-color:#001529;">
+        <div
+            style="height: 60px; line-height: 60px; color: white;display: flex; align-items: center; justify-content: center">
+          logo
+        </div>
+
+        <el-menu :collapse="isCollapse" :collapse-transition="false" router background-color="#001529"
+                 text-color="rgba(255, 255, 255, 0.65)"
+                 active-text-color="#fff"
+                 style="border: none" :default-active="$route.path">
+          <el-menu-item index="/">
+            <template>
+              <i class="el-icon-house"></i>
+              <span slot="title">Home page</span>
+            </template>
+          </el-menu-item>
+          <el-menu-item index="/element">Element</el-menu-item>
+          <el-menu-item>Home</el-menu-item>
+          <el-menu-item>Home</el-menu-item>
+          <el-submenu>
+            <template slot="title">
+              <i class="el-icon-menu"></i>
+              <span>Management</span>
+            </template>
+            <el-menu-item>User information</el-menu-item>
+            <el-menu-item>Manager information</el-menu-item>
+            <el-menu-item>Staff information</el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+
+      <el-container>
+        <!--      header-->
+        <el-header>
+          <i :class="collapseIcon" style="font-size: 26px" @click="handleCollapse"></i>
+          <el-breadcrumb separator-class="el-icon-arrow-right" style="margin-left: 20px">
+            <el-breadcrumb-item :to="{ path: '/' }">Home</el-breadcrumb-item>
+            <el-breadcrumb-item :to="{ path: '/user' }">User management</el-breadcrumb-item>
+          </el-breadcrumb>
+
+          <div style="flex: 1; width: 0; display: flex; align-items: center; justify-content: flex-end">
+            <el-dropdown>
+              <div style="display: flex; align-items: center">
+                <img src="@/assets/logo.png" alt="" style="width: 40px; height: 40px">
+                <span>Admin</span>
+              </div>
+              <el-dropdown-menu slot="dropdown">
+                <el-dropdown-item>Personal information</el-dropdown-item>
+                <el-dropdown-item>Change password</el-dropdown-item>
+                <el-dropdown-item @click="">Exit</el-dropdown-item>
+              </el-dropdown-menu>
+            </el-dropdown>
+          </div>
+        </el-header>
+        <!--        main-->
+        <el-main>
+          <div style="box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); padding: 10px 20px; border-radius: 5px; margin-buttom: 10px">
+            Hello world!!
+          </div>
+          <el-card style="width: 500px">
+            <div slot="header" class="clearfix">
+              <span>vue demo</span>
+            </div>
+            <div>
+              framework
+              <div style="margin-top: 20px">
+                <div style="margin: 10px 0"><strong>Main theme</strong></div>
+                <el-button type="primary">button</el-button>
+                <el-button type="success">button</el-button>
+                <el-button type="info">button</el-button>
+                <el-button type="warning">button</el-button>
+              </div>
+            </div>
+
+          </el-card>
+        </el-main>
+
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeView',
+  data() {
+    return {
+      isCollapse: false,
+      asideWidth: '200px',
+      collapseIcon: 'el-icon-s-fold'
+    }
+  },
+  methods: {
+    handleCollapse() {
+      this.isCollapse = !this.isCollapse
+      this.asideWidth = this.isCollapse ? '64px' : '200px'
+      this.collapseIcon = this.isCollapse ? 'el-icon-s-unfold' : 'el-icon-s-fold'
+    }
+  }
+}
+</script>
+
+<style>
+.el-menu--inline {
+  background-color: #000c17 !important;
+}
+
+.el-menu--inline .el-menu-item {
+  background-color: #000c17 !important;
+  padding-left: 49px !important;
+}
+
+.el-menu-item:hover, .el-submenu__title:hover {
+  color: #fff !important;
+}
+
+.el-submenu__title:hover i {
+  color: #fff !important;
+}
+
+.el-menu-item.is-active {
+  background-color: #1890ff !important;
+  border-radius: 5px !important;
+  margin: 4px !important;
+}
+
+.el-menu-item {
+  margin: 4px !important;
+  height: 40px !important;
+  line-height: 40px !important;
+}
+
+.el-submenu__title {
+  margin: 4px !important;
+  height: 40px !important;
+  line-height: 40px !important;
+}
+
+.el-submenu__icon-arrow {
+  margin-top: -5px;
+}
+
+.el-header {
+  box-shadow: 2px 0 6px rgba(0, 21, 41, 0.35);
+  display: flex;
+  align-items: center;
+}
+</style>
